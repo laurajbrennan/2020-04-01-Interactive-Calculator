5e0491e10847a648bd0db7a2bfec67b6107f95ee
ellejaiebaie <48771627+laurajbrennan@users.noreply.github.com>
2020-04-01 10:01:40 -0700

    first commit of the app

---
+++ calculator/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Text Stats</h1>
+      <p>
+        Type or paste your text here for some vital statistics on your writing!
+      </p>
+      <textarea></textarea>
+    </div>
+  );
+}
+
+export default App;
